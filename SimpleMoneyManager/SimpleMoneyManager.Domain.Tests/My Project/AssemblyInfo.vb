f760534d672dde1189e886915eaf8140511bc382
StudioPasokon <andy@studiopasokon.com>
2020-06-21 16:38:02 +0200

    Create unit test project for domain entities

---
+++ SimpleMoneyManager/SimpleMoneyManager.Domain.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("SimpleMoneyManager.Domain.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SimpleMoneyManager.Domain.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("a1c83cdc-d59d-49bd-9775-6033b99736c3")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ SimpleMoneyManager/SimpleMoneyManager.Domain.Tests/TransactionTests.vb
@@ -0,0 +1,26 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+Imports SimpleMoneyManager.Domain.SimpleMoneyManager.Domain
+
+' TODO: complete unit test.
+
+Namespace SimpleMoneyManager.Domain.Tests
+    ''' <summary>
+    ''' Test class that checks the internals of the <see cref="Transaction"/> class.
+    ''' </summary>
+    <TestClass()> Public Class TransactionTests
+
+        <TestMethod()> Public Sub TestMethod1()
+            ' Arrange.
+
+            ' Act.
+
+            ' Assert.
+        End Sub
+
+    End Class
+
+End Namespace
