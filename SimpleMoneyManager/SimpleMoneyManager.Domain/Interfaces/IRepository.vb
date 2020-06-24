d7b18aa8d01b6f4bfe1ea2796155a4db45787439
StudioPasokon <andy@studiopasokon.com>
2020-06-24 21:41:39 +0200

    Add repository interface to the domain for reading and writing data

---
+++ SimpleMoneyManager/SimpleMoneyManager.Domain/Interfaces/IRepository.vb
@@ -0,0 +1,15 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+Namespace SimpleMoneyManager.Domain.Interfaces
+
+    ''' <summary>
+    ''' Interface class defining the access to the data repository of the application.
+    ''' </summary>
+    Public Interface IRepository
+
+    End Interface
+
+End Namespace
