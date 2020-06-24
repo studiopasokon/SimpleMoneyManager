ed450e1493591113ed850e2fe2d3e76e23657a90
StudioPasokon <andy@studiopasokon.com>
2020-06-24 21:42:23 +0200

    Add infrastructure project for the text file repository implementation

---
+++ SimpleMoneyManager/SimpleMoneyManager.Infrastructure/TextFileRepository.vb
@@ -0,0 +1,17 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+Imports SimpleMoneyManager.Domain.SimpleMoneyManager.Domain.Interfaces
+
+Namespace SimpleMoneyManager.Infrastructure
+
+    ''' <summary>
+    ''' Simple text file repository implementation to store the financial information.
+    ''' </summary>
+    Public Class TextFileRepository : Implements IRepository
+
+    End Class
+
+End Namespace
