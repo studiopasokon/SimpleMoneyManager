55d0f519641f98d045aa15247b50914d7422de07
StudioPasokon <andy@studiopasokon.com>
2020-06-28 16:50:30 +0200

    Modify the initial old text file repository class to an FinancialYearRepository class in the proper namespace

---
+++ SimpleMoneyManager/SimpleMoneyManager.Infrastructure/Repositories/FinancialYearRepository.vb
@@ -0,0 +1,17 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+Imports SimpleMoneyManager.Domain.Interfaces
+
+Namespace Repositories
+
+    ''' <summary>
+    ''' Repository storing financial year information.
+    ''' </summary>
+    Public Class FinancialYearRepository : Implements IRepository
+
+    End Class
+
+End Namespace
