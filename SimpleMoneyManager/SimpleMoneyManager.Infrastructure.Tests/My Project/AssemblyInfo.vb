2f717eabd708a20f14f245f245e2202c03052443
StudioPasokon <andy@studiopasokon.com>
2020-07-04 20:49:24 +0200

    Add test project for testing the Infrastructure assembly

---
+++ SimpleMoneyManager/SimpleMoneyManager.Infrastructure.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("SimpleMoneyManager.Infrastructure.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SimpleMoneyManager.Infrastructure.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("b6e20409-63d5-4e08-b9d9-3405834a84ab")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
