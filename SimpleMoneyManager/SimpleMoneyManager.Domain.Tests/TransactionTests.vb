f1c52229b2287eb285e9de0f0f0e1897f56cb586
StudioPasokon <andy@studiopasokon.com>
2020-07-04 20:52:01 +0200

    Remove unneeded namespace

--- SimpleMoneyManager/SimpleMoneyManager.Domain.Tests/TransactionTests.vb
+++ SimpleMoneyManager/SimpleMoneyManager.Domain.Tests/TransactionTests.vb
@@ -23,294 +23,291 @@ Option Strict On
 Imports SimpleMoneyManager.Domain.Entities
 Imports SimpleMoneyManager.Domain.ValueTypes
 
-Namespace EntityTests
 
-    ''' <summary>
-    ''' Test class that checks the internals of the <see cref="Transaction"/> entity.
-    ''' </summary>
-    <TestClass()> Public Class TransactionTests
+''' <summary>
+''' Test class that checks the internals of the <see cref="Transaction"/> entity.
+''' </summary>
+<TestClass()> Public Class TransactionTests
 
 #Region "Equals tests"
 
-        <TestMethod()> Public Sub Transactions_ShouldBeEqual_WhenSameObject()
-            ' Arrange.
-            Dim transaction As New Transaction()
-            With transaction
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_ShouldBeEqual_WhenSameObject()
+        ' Arrange.
+        Dim transaction As New Transaction()
+        With transaction
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction.Equals(transaction)
+        ' Act.
+        Dim equalResult = transaction.Equals(transaction)
 
-            ' Assert.
-            Assert.IsTrue(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsTrue(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_ShouldBeEqual_WhenSimilarObjects()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_ShouldBeEqual_WhenSimilarObjects()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            Dim transaction2 As New Transaction()
-            With transaction2
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+        Dim transaction2 As New Transaction()
+        With transaction2
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1.Equals(transaction2)
+        ' Act.
+        Dim equalResult = transaction1.Equals(transaction2)
 
-            ' Assert.
-            Assert.IsTrue(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsTrue(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_ShouldBeNotEqual_WhenOneObjectIsNothing()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_ShouldBeNotEqual_WhenOneObjectIsNothing()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1.Equals(Nothing)
+        ' Act.
+        Dim equalResult = transaction1.Equals(Nothing)
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_ShouldBeNotEqual_WhenObjectsAreDifferent()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_ShouldBeNotEqual_WhenObjectsAreDifferent()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            Dim transaction2 As New Transaction()
-            With transaction2
-                .Amount = 200
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Payment
-            End With
+        Dim transaction2 As New Transaction()
+        With transaction2
+            .Amount = 200
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Payment
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1.Equals(transaction2)
+        ' Act.
+        Dim equalResult = transaction1.Equals(transaction2)
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transaction_ShouldBeNotEqual_WhenObjectOfWrongType()
-            ' Arrange.
-            Dim transaction As New Transaction()
-            With transaction
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transaction_ShouldBeNotEqual_WhenObjectOfWrongType()
+        ' Arrange.
+        Dim transaction As New Transaction()
+        With transaction
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction.Equals("compare string")
+        ' Act.
+        Dim equalResult = transaction.Equals("compare string")
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
 #End Region
 
 #Region "Operator = tests"
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldBeEqual_WhenSameObject()
-            ' Arrange.
-            Dim transaction As New Transaction()
-            With transaction
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldBeEqual_WhenSameObject()
+        ' Arrange.
+        Dim transaction As New Transaction()
+        With transaction
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction = transaction
+        ' Act.
+        Dim equalResult = transaction = transaction
 
-            ' Assert.
-            Assert.IsTrue(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsTrue(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldBeEqual_WhenSimilarObjects()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldBeEqual_WhenSimilarObjects()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            Dim transaction2 As New Transaction()
-            With transaction2
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+        Dim transaction2 As New Transaction()
+        With transaction2
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1 = transaction2
+        ' Act.
+        Dim equalResult = transaction1 = transaction2
 
-            ' Assert.
-            Assert.IsTrue(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsTrue(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldBeNotEqual_WhenOneObjectIsNothing()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldBeNotEqual_WhenOneObjectIsNothing()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1 = Nothing
+        ' Act.
+        Dim equalResult = transaction1 = Nothing
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldBeNotEqual_WhenObjectsAreDifferent()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldBeNotEqual_WhenObjectsAreDifferent()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            Dim transaction2 As New Transaction()
-            With transaction2
-                .Amount = 200
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Payment
-            End With
+        Dim transaction2 As New Transaction()
+        With transaction2
+            .Amount = 200
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Payment
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1 = transaction2
+        ' Act.
+        Dim equalResult = transaction1 = transaction2
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
 #End Region
 
 #Region "Operator <> tests"
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldNotBeDifferent_WhenSameObject()
-            ' Arrange.
-            Dim transaction As New Transaction()
-            With transaction
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldNotBeDifferent_WhenSameObject()
+        ' Arrange.
+        Dim transaction As New Transaction()
+        With transaction
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction <> transaction
+        ' Act.
+        Dim equalResult = transaction <> transaction
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldNotBeDifferent_WhenSimilarObjects()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldNotBeDifferent_WhenSimilarObjects()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            Dim transaction2 As New Transaction()
-            With transaction2
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+        Dim transaction2 As New Transaction()
+        With transaction2
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1 <> transaction2
+        ' Act.
+        Dim equalResult = transaction1 <> transaction2
 
-            ' Assert.
-            Assert.IsFalse(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsFalse(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldBeDifferent_WhenOneObjectIsNothing()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldBeDifferent_WhenOneObjectIsNothing()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1 <> Nothing
+        ' Act.
+        Dim equalResult = transaction1 <> Nothing
 
-            ' Assert.
-            Assert.IsTrue(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsTrue(equalResult)
+    End Sub
 
-        <TestMethod()> Public Sub Transactions_Operator_ShouldBeDifferent_WhenObjectsAreDifferent()
-            ' Arrange.
-            Dim transaction1 As New Transaction()
-            With transaction1
-                .Amount = 100
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Receipt
-            End With
+    <TestMethod()> Public Sub Transactions_Operator_ShouldBeDifferent_WhenObjectsAreDifferent()
+        ' Arrange.
+        Dim transaction1 As New Transaction()
+        With transaction1
+            .Amount = 100
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Receipt
+        End With
 
-            Dim transaction2 As New Transaction()
-            With transaction2
-                .Amount = 200
-                .Description = "Test transaction"
-                .TransactionDate = Date.Now
-                .TransferType = TransactionType.Payment
-            End With
+        Dim transaction2 As New Transaction()
+        With transaction2
+            .Amount = 200
+            .Description = "Test transaction"
+            .TransactionDate = Date.Now
+            .TransferType = TransactionType.Payment
+        End With
 
-            ' Act.
-            Dim equalResult = transaction1 <> transaction2
+        ' Act.
+        Dim equalResult = transaction1 <> transaction2
 
-            ' Assert.
-            Assert.IsTrue(equalResult)
-        End Sub
+        ' Assert.
+        Assert.IsTrue(equalResult)
+    End Sub
 
 #End Region
 
-    End Class
-
-End Namespace
+End Class
