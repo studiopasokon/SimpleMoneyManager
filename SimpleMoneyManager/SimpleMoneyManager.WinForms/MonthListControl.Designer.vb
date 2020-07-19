14e8f8df7dd59afd0ba337d7611bd16cf3355209
StudioPasokon <andy@studiopasokon.com>
2020-07-19 14:47:41 +0200

    Add extra column to make up the difference between expenses and income

--- SimpleMoneyManager/SimpleMoneyManager.WinForms/MonthListControl.Designer.vb
+++ SimpleMoneyManager/SimpleMoneyManager.WinForms/MonthListControl.Designer.vb
@@ -26,12 +26,14 @@ Partial Class MonthListControl
         Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.MonthNameLabel = New System.Windows.Forms.Label()
         Me.TransactionDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.TransactionDescription = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TransactionAmount = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TransactionAmountPayed = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TransactionAmountReceived = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.EditButton = New System.Windows.Forms.DataGridViewButtonColumn()
         Me.DeleteButton = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.MonthNameLabel = New System.Windows.Forms.Label()
+        Me.AddTransactionButton = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -39,23 +41,14 @@ Partial Class MonthListControl
         '
         Me.DataGridView1.AllowUserToDeleteRows = False
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.TransactionDate, Me.TransactionDescription, Me.TransactionAmount, Me.EditButton, Me.DeleteButton})
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.TransactionDate, Me.TransactionDescription, Me.TransactionAmountPayed, Me.TransactionAmountReceived, Me.EditButton, Me.DeleteButton})
         Me.DataGridView1.Location = New System.Drawing.Point(5, 34)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
         Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(716, 172)
+        Me.DataGridView1.Size = New System.Drawing.Size(714, 216)
         Me.DataGridView1.TabIndex = 0
-        '
-        'MonthNameLabel
-        '
-        Me.MonthNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MonthNameLabel.Location = New System.Drawing.Point(3, 0)
-        Me.MonthNameLabel.Name = "MonthNameLabel"
-        Me.MonthNameLabel.Size = New System.Drawing.Size(307, 31)
-        Me.MonthNameLabel.TabIndex = 1
-        Me.MonthNameLabel.Text = "MonthName"
         '
         'TransactionDate
         '
@@ -77,15 +70,23 @@ Partial Class MonthListControl
         Me.TransactionDescription.ReadOnly = True
         Me.TransactionDescription.Width = 125
         '
-        'TransactionAmount
+        'TransactionAmountPayed
         '
         DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
-        Me.TransactionAmount.DefaultCellStyle = DataGridViewCellStyle3
-        Me.TransactionAmount.HeaderText = "Amount"
-        Me.TransactionAmount.MinimumWidth = 6
-        Me.TransactionAmount.Name = "TransactionAmount"
-        Me.TransactionAmount.ReadOnly = True
-        Me.TransactionAmount.Width = 125
+        Me.TransactionAmountPayed.DefaultCellStyle = DataGridViewCellStyle3
+        Me.TransactionAmountPayed.HeaderText = "Amount Payed"
+        Me.TransactionAmountPayed.MinimumWidth = 6
+        Me.TransactionAmountPayed.Name = "TransactionAmountPayed"
+        Me.TransactionAmountPayed.ReadOnly = True
+        Me.TransactionAmountPayed.Width = 125
+        '
+        'TransactionAmountReceived
+        '
+        Me.TransactionAmountReceived.HeaderText = "Amount Received"
+        Me.TransactionAmountReceived.MinimumWidth = 6
+        Me.TransactionAmountReceived.Name = "TransactionAmountReceived"
+        Me.TransactionAmountReceived.ReadOnly = True
+        Me.TransactionAmountReceived.Width = 125
         '
         'EditButton
         '
@@ -106,14 +107,33 @@ Partial Class MonthListControl
         Me.DeleteButton.Text = "Delete"
         Me.DeleteButton.Width = 50
         '
+        'MonthNameLabel
+        '
+        Me.MonthNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MonthNameLabel.Location = New System.Drawing.Point(3, 0)
+        Me.MonthNameLabel.Name = "MonthNameLabel"
+        Me.MonthNameLabel.Size = New System.Drawing.Size(307, 31)
+        Me.MonthNameLabel.TabIndex = 1
+        Me.MonthNameLabel.Text = "MonthName"
+        '
+        'AddTransactionButton
+        '
+        Me.AddTransactionButton.Location = New System.Drawing.Point(596, 256)
+        Me.AddTransactionButton.Name = "AddTransactionButton"
+        Me.AddTransactionButton.Size = New System.Drawing.Size(123, 23)
+        Me.AddTransactionButton.TabIndex = 2
+        Me.AddTransactionButton.Text = "&Add Transaction"
+        Me.AddTransactionButton.UseVisualStyleBackColor = True
+        '
         'MonthListControl
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.AddTransactionButton)
         Me.Controls.Add(Me.MonthNameLabel)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "MonthListControl"
-        Me.Size = New System.Drawing.Size(786, 213)
+        Me.Size = New System.Drawing.Size(786, 291)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -123,7 +143,9 @@ Partial Class MonthListControl
     Friend WithEvents MonthNameLabel As Label
     Friend WithEvents TransactionDate As DataGridViewTextBoxColumn
     Friend WithEvents TransactionDescription As DataGridViewTextBoxColumn
-    Friend WithEvents TransactionAmount As DataGridViewTextBoxColumn
+    Friend WithEvents TransactionAmountPayed As DataGridViewTextBoxColumn
+    Friend WithEvents TransactionAmountReceived As DataGridViewTextBoxColumn
     Friend WithEvents EditButton As DataGridViewButtonColumn
     Friend WithEvents DeleteButton As DataGridViewButtonColumn
+    Friend WithEvents AddTransactionButton As Button
 End Class
